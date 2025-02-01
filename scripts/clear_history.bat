44a0bdb57114d7cc62b0500bedbc32b1d76cbd44
ShenMian <sms_school@outlook.com>
2025-02-01 18:03:12 +0000

    .

--- scripts/clear_history.bat
+++ scripts/clear_history.bat
@@ -2,11 +2,11 @@
 
 cd %~dp0/.. || exit /b 1
 
-git checkout --orphan empty        || exit /b 1
-git branch -D main                 || exit /b 1
-git add -A                         || exit /b 1
-git commit -m "."                  || exit /b 1
-git push origin empty:main --force || exit /b 1
-git checkout main                  || exit /b 1
+git checkout --orphan empty          || exit /b 1
+git branch -D main                   || exit /b 1
+git add -A                           || exit /b 1
+git commit -m "feat: initial commit" || exit /b 1
+git push origin empty:main --force   || exit /b 1
+git checkout main                    || exit /b 1
 git branch -D empty
 git pull origin main --allow-unrelated-histories
--- scripts/clear_history.sh
+++ scripts/clear_history.sh
@@ -2,11 +2,11 @@
 
 cd "$( cd "$( dirname "$0"  )" && pwd  )/.." || exit
 
-git checkout --orphan empty        || exit 1
-git branch -D main                 || exit 1
-git add -A                         || exit 1
-git commit -m '.'                  || exit 1
-git push origin empty:main --force || exit 1
-git checkout main                  || exit 1
+git checkout --orphan empty          || exit 1
+git branch -D main                   || exit 1
+git add -A                           || exit 1
+git commit -m 'feat: initial commit' || exit 1
+git push origin empty:main --force   || exit 1
+git checkout main                    || exit 1
 git branch -D empty
 git pull origin main --allow-unrelated-histories
